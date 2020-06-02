78d3e7567e5698204901d68843cc3bf4c8ebc32d
Yariv Garala <yariv1052@gmail.com>
2020-06-02 18:43:47 +0300

    update: create user & querie data from DB

--- src/queries.js
+++ src/queries.js
@@ -1,4 +1,3 @@
-
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'yariv',
@@ -7,6 +6,91 @@ const pool = new Pool({
     password: 'Nikol#1805',
     port: 5432,
 });
+
+
+//-----------------------------------------------------------------------------
+
+
+/*
+ * Running the user query from the "Search engine".
+ */
+const run_query = async (query) => {
+    let usersData;
+    try {
+        usersData = await pool.query(query)
+    } catch (e) {
+        console.log(e)
+    }
+    return usersData;
+};
+
+
+//-----------------------------------------------------------------------------
+
+
+/*
+ * Create a new user in the DB.
+ */
+const createUser = async (email, pass) => {
+   let userExist;
+
+   // Check if user already exist in our DB.
+    try{
+        userExist = await run_query("select * from users where email = '" + email + "' and pass = '" + pass + "'");
+    }
+    catch (e) {
+        console.log(e);
+    }
+
+    // if user dose not exist
+    if (userExist.rows.length < 1) {
+        try {
+            await pool.query('INSERT INTO users (email, pass)' +
+                ' VALUES ($1, $2)', [email, pass]);
+        } catch (e) {
+            console.log(e)
+        }
+        return "succeeded";
+    }
+    //if user exist
+    return "User already exists"
+};
+
+
+//-----------------------------------------------------------------------------
+
+
+/*
+ * Delete specific user.
+ */
+const deleteUser = async (email) => {
+    let userExist;
+
+    // Check if user already exist in our DB.
+    try {
+        userExist = await run_query("select * from users where email ='" + email + "'");
+    } catch (e) {
+        console.log(e);
+    }
+
+    // if user already exist we will delete it
+    if (userExist.rows.length >= 1) {
+        try {
+            let queryForDelete = await pool.query('DELETE from users where' +
+                ' email = $1 ', [email])
+        } catch (e) {
+            console.log(e)
+        }
+        return "User was deleted";
+    }
+
+    // if user dose not exist
+    return "User dose not exist";
+};
+
+
+//-----------------------------------------------------------------------------
+
 
 //GET: multi users
 // inside the pool.query() we put the raw SQL that will touch the api database.
@@ -20,6 +104,7 @@ const getUsers = (request, response) => {
     console.log(usersData);
 };
 
+
 //GET: single user
 // inside the pool.query() we put the raw SQL that will touch the api database.
 const getUserByPass = (request, response) => {
@@ -29,22 +114,24 @@ const getUserByPass = (request, response) => {
         if (error) {
             throw error
         }
-        response.status(200).json(results.rows)
+        //response.status(200).json(results.rows)
     })
-}
+};
+
 
 //POST: in this function, we’re extracting the name and email properties from the request body, and INSERTing the values.
 // inside the pool.query() we put the raw SQL that will touch the api database.
-const createUser = (request, response) => {
-    const {username, email} = request.body
+// const createUser = (request, response) => {
+//      const {username, email} = request.body;
+//
+//     pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [username, email], (error, results) => {
+//         if (error) {
+//             throw error
+//         }
+//         // response.status(201).send(`User added with ID: ${result.insertId}`)
+//     })
+// };
 
-    pool.query('INSERT INTO users (username, email) VALUES ($1, $2)', [username, email], (error, results) => {
-        if (error) {
-            throw error
-        }
-        // response.status(201).send(`User added with ID: ${result.insertId}`)
-    })
-}
 
 //PUT: for this query, we’ll combine GET and POST to use the UPDATE clause.
 // inside the pool.query() we put the raw SQL that will touch the api database.
@@ -64,17 +151,21 @@ const updateUser = (request, response) => {
     )
 }
 
-//delete a specific user by password.
-const deleteUser = (request, response) => {
-    const pass = parseInt(request.params.password)
 
-    pool.query('DELETE FROM users WHERE pass = $1', [pass], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).send(`User deleted with PASSWORD: ${pass}`)
-    })
-}
+// Delete a specific user by password.
+// const deleteUser = (request, response) => {
+//     const pass = parseInt(request.params.password)
+//
+//     pool.query('DELETE FROM users WHERE pass = $1', [pass], (error, results) => {
+//         if (error) {
+//             throw error
+//         }
+//         response.status(200).send(`User deleted with PASSWORD: ${pass}`)
+//     })
+// }
+
+//-----------------------------------------------------------------------------
+
 
 //in order to access these functions from server.js, we’ll export them.
 module.exports = {
@@ -83,4 +174,5 @@ module.exports = {
     createUser,
     updateUser,
     deleteUser,
-}
+    run_query
+};
--- src/server.js
+++ src/server.js
@@ -4,33 +4,114 @@ const bodyParser = require('body-parser');
 const db = require('./queries');
 const app_port = process.env.PORT || 3000;
 const path = require('path');
+const client = require("express");
 
 //
-app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: true,
     })
 );
+app.use(bodyParser.json());
+
+
+// define 'public' folder as static
+// app.use(express.static("src"));
 
 
 // set the views engine to ejs
 app.set("view engine", "ejs")
-app.set("views", path.join(__dirname + "/views"))
+app.set("views", path.join(__dirname + "/views"));
+
 
 //Home page
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     // response.json({ info: 'Node.js, Express, and Postgres API' })
     res.render('pages/index');
 });
 
 
+// Getting user login details
+// app.post('/login', (req, res) => {
+//     const email = req.body.email
+//     const password = req.body.password
+//
+//     console.log("User email is: " + email + "\nUser password is: " + password);
+//
+//
+//     res.send("login")
+// });
+
+
+// Sign up to the website - user data will be inserted to the DB.
+app.post('/signup', async (req, res) => {
+    let results = {};
+    results.row = [];
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    console.log("User email is: " + email + "\nUser password is: " + password);
+
+    // insert the user data to the data base
+    results = await db.createUser(email, password);
+    console.log(results);
+    res.send(results);
+});
+
+
+
 // about page
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
     res.render('pages/about');
 });
 
 
+// search a user in our PGDataBase
+// this function will allow us to make SQLInjection
+app.get('/search', async (req, res) => {
+    let results = {};
+    results.row = [];
+    try {
+        //takes the input from the web => WRONG IDEA!!! -> we must to check
+        // the input!
+        const userString = req.query.search_word;
+        console.log("User input is: " + userString);
+        const query = `select * from users where email = '${userString}'`;
+        console.log("Full query is: " + query);
+
+        // The data we query from the DB
+        results = await db.run_query(query);
+        console.log(results.rows);
+    } catch (e) {
+        console.log("Error");
+    }
+    // finally {
+    //     res.setHeader("content-type", "application/json")
+    //     res.send(JSON.stringify(results.row));
+    // }
+    res.send(results.rows);
+    // res.render('page/index', {results: results.rows});
+});
+
+
+// send the email of the user to queries.js file for deleting
+app.get('/userToDelete', async (req, res) => {
+    let results = {};
+    results.row = [];
+    try {
+        const userEmail = req.query.userToDelete;
+        console.log("The user " + userEmail + " will be deleted!");
+
+        results = await db.deleteUser(userEmail);
+        console.log(results);
+    } catch (e) {
+        console.log("Error");
+    } finally {
+        res.setHeader("content-type", "application/json")
+        res.send(JSON.stringify(results));
+    }
+});
 
 app.get('/users', db.getUsers);
 app.get('/users/:pass', db.getUserByPass);
